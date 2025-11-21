b3d0583d9cc13195bc93e7006b9e2beb45018557
phLinux <paulohtolotti@alumni.usp.br>
2025-11-21 14:12:29 -0300

    feat: daojdbc basic implementations

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,4 +1,37 @@
 package model.dao.impl;
 
-public class DepartmentDaoJDBC {
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    public DepartmentDaoJDBC() {
+
+    }
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
+    }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,4 +1,57 @@
 package model.dao.impl;
 
-public class SellerDaoJDBC {
+import db.DB;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    public SellerDaoJDBC() {
+
+    }
+    @Override
+    public void insert(Seller obj) throws SQLException{
+        PreparedStatement ps = null;
+        try {
+
+            Connection conn = DB.openConnection();
+
+            ps = conn.prepareStatement("INSERT INTO Seller VALUES" +
+                    "(?, ?, ?, ?, ?, ?)");
+
+            int rows = ps.executeUpdate();
+            if(rows > 0) System.out.println("Added " + rows + " successfully");
+
+        } catch (SQLException err) {
+
+        } finally {
+            DB.closeStatement(ps);
+            DB.closeConnection();
+        }
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
 }
