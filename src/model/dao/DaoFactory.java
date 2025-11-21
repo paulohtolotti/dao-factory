207311297bc1f27018d3ca7700702169faf78357
phLinux <paulohtolotti@alumni.usp.br>
2025-11-21 15:27:17 -0300

    fix: dao factory dependency update

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,15 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
+
+import static db.DB.conn;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.openConnection());
     }
 
     public static DepartmentDao createDepartmentDao() {
