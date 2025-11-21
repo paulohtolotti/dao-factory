0a9026fe7975bb3b29e6e551c51366ece4ffb8ed
phLinux <paulohtolotti@alumni.usp.br>
2025-11-21 15:46:40 -0300

    feat: findById method implementation

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,11 +1,22 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private Connection conn;
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
 
     public DepartmentDaoJDBC() {
 
@@ -27,7 +38,32 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public Department findById(Integer id) {
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            ps = this.conn.prepareStatement("SELECT * FROM department WHERE Id = ?");
+
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+
+            if(rs.next()) {
+                return new Department(rs.getInt("Id"), rs.getString("Name"));
+            }
+
+        } catch (SQLException err) {
+            throw new DbException(err.getMessage());
+
+        } finally {
+
+            DB.closeResultSet(rs);
+            DB.closeStatement(ps);
+        }
+
         return null;
+
     }
 
     @Override
