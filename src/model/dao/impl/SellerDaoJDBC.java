57a711b351f6d09f7fac683f215fcee484b7b9b8
phLinux <paulohtolotti@alumni.usp.br>
2025-11-23 08:07:08 -0300

    feat: findByDepartment operation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -10,7 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
 
@@ -69,6 +72,48 @@ public class SellerDaoJDBC implements SellerDao {
         }
     }
 
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public List<Seller> findByDepartment(Department d) {
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<Seller> sellers = new ArrayList<>();
+        try {
+            ps = conn.prepareStatement("SELECT seller.*, department.Id as DepId, department.Name as DepName " +
+                    "FROM seller JOIN department ON department.Id = seller.DepartmentId WHERE " +
+                    "department.Id = ? ORDER BY Name");
+
+            ps.setInt(1, d.getId());
+            rs = ps.executeQuery();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while(rs.next()) {
+
+                Department dep = map.get(rs.getInt("DepId"));
+
+                if(dep == null) {
+                    map.put(rs.getInt("DepId"), instantiateDepartment(rs));
+                }
+
+                sellers.add(instantiateSeller(rs, map.get(rs.getInt("DepId"))));
+            }
+
+            return sellers;
+
+        } catch(SQLException err) {
+            throw new DbException(err.getMessage());
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(ps);
+        }
+
+    }
+
     private Seller instantiateSeller(ResultSet rs, Department d) throws SQLException {
         return new Seller(rs.getInt("Id"), d, rs.getString("Name"), rs.getString("Email"), rs.getDate("BirthDate")
                 .toLocalDate(),rs.getDouble("BaseSalary"));
@@ -78,8 +123,5 @@ public class SellerDaoJDBC implements SellerDao {
         return new Department(rs.getInt("DepartmentId"), rs.getString(8));
     }
 
-    @Override
-    public List<Seller> findAll() {
-        return List.of();
-    }
+
 }
